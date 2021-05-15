c4b7ce1f3904a82b3b38abda596283486e289a67
AyamiYokoi <didufd63hff@gmail.com>
2021-05-15 23:50:39 +0000

    [Add]create

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -10,8 +10,16 @@ class MemoriesController < ApplicationController
   end
   
   def create
+    memory = Memory.new(memory_params)
+    memory.save
+    redirect_to memory_path(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  def memory_params
+    params.require(:memory).permit(:title, :body)
   end
 end
