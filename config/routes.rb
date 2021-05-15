c6acd6084448b2444bcb64e467b71d599d6541db
AyamiYokoi <didufd63hff@gmail.com>
2021-05-15 03:55:07 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'memories/top'
+  get 'memories/index'
+  get 'memories/show'
+  get 'memories/new'
+  get 'memories/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
